6cb9344cee3d8b7c48d889a800488a0f56bceac3
xiaoxiaogao <gaoMK85@163.com>
2026-02-20 09:37:41 +0800

    2841. 几乎唯一子数组的最大和

---
+++ Two Pointers&Sliding Window/Fixed-length Sliding Window/2841. 几乎唯一子数组的最大和/2841. 几乎唯一数组的最大和.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long maxSum(vector<int>& nums, int m, int k) { unordered_map<int,int> unmap;
+    long long sum=0,ans=0;
+    for(int i=0;i<k;i++){
+        unmap[nums[i]]++;
+        sum+=nums[i];
+        if(unmap[nums[i]]==1){
+            m--;
+        }
+    }
+    if(m<=0){
+        ans=max(ans,sum);
+    }
+    for(int i=0;i<nums.size()-k;i++){
+        unmap[nums[i]]--;
+        unmap[nums[i+k]]++;
+        if(unmap[nums[i]]==0&&nums[i]!=nums[i+k])m++;
+if(unmap[nums[i+k]]==1&&nums[i]!=nums[i+k])m--;
+sum=sum+nums[i+k]-nums[i];
+if(m<=0){
+ans=max(ans,sum);
+}else{
+    continue;
+}        
+    }
+    return ans;
+    
+    }
+};
