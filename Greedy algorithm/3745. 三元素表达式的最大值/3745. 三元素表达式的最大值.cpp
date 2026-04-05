b98472d7f0dd08cb6ca2691966c05c6724a36962
xiaoxiaogao <gaoMK85@163.com>
2026-04-05 18:09:33 +0800

    3745. 三元素表达式的最大值

---
+++ Greedy algorithm/3745. 三元素表达式的最大值/3745. 三元素表达式的最大值.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int maximizeExpressionOfThree(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int len = nums.size();
+        return nums[len-1]+nums[len-2]-nums[0];
+    }
+};
